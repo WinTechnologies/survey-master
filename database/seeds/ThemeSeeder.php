de0bf14f202467548e9ff8d24dfa10d9379665ca
TrustBiz <60724108+ChenSeeSai@users.noreply.github.com>
2020-11-28 00:20:57 +0800

    default theme

--- database/seeds/ThemeSeeder.php
+++ database/seeds/ThemeSeeder.php
@@ -12,6 +12,20 @@ class ThemeSeeder extends Seeder
     public function run()
     {
         DB::table('themes')->insert([
+            'name' => 'Default',
+            'img_url' => '',
+            'size_ans' => 24,
+            'size_ques' => 24,
+            'text_color' => '#37404a',
+            'border_color' => 'rgba(92,92,92,0.6)',
+            'size_ans_img' => 24,
+            'background_color' => '#FFFFFF',
+            'button_color' => '#37404a',
+            'footer_color' => '#ededed',
+            'font_family' => 'Karla'
+        ]);
+
+        DB::table('themes')->insert([
             'name' => 'Banana',
             'img_url' => '',
             'size_ans' => 24,
